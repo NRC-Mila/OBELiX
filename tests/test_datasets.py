159739c1c887f4a0a98f8bf5fd870a17861c78dc
leah <leah85503@gmail.com>
2025-05-14 17:04:45 +0300

    Added more assertions to the test methods in test_datasets script

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -4,9 +4,15 @@ from obelix import LiIon, OBELiX, Laskowski, ShonAndMin, Dataset
 def test_LiIon():
     shutil.rmtree("lilon_rawdata", ignore_errors=True)
     obelix_object = OBELiX()
+    
+    li_raw = LiIon(room_temp_only=False)
+    assert len(li_raw.dataframe) == 820
+    
     li = LiIon()
     assert len(li.dataframe) == 450
+    
     assert 'Reduced Composition' in li.dataframe.columns
+    
     assert len(li.remove_obelix(obelix_object)) == 297
 
 def test_Laskowski():
@@ -14,26 +20,34 @@ def test_Laskowski():
     obelix_object = OBELiX()
 
     la = Laskowski()
-
     assert 'Reduced Composition' in la.dataframe.columns
 
     assert len(la.dataframe) == 1346
+
     assert len(la.remove_obelix(obelix_object)) == 824
+
 
 def test_ShonAndMin():
     shutil.rmtree("SM_rawdata", ignore_errors=True)
 
+    sm_raw = ShonAndMin(clean_data=False)
+    assert len(sm_raw.dataframe) == 4032  
+
     sm = ShonAndMin()
+    assert len(sm.dataframe) == 3083 
 
-    assert len(sm.dataframe) == 3083
+def test_merge_datasets():
 
-def test_Dataset():
-    
     ob = OBELiX()
-    li = LiIon ()
-    la = Laskowski ()
+    li = LiIon()
+    la = Laskowski()
     sm = ShonAndMin()
-    
-    df = Dataset.merge_datasets(ob, li, la, sm)
-    assert df.shape[0] == 4316  
+
+    assert len(Dataset.merge_datasets(ob, li, la, sm, remove_duplicates=False)) == 5478
+    assert len(Dataset.merge_datasets(ob, li, la, sm)) == 4316
+    assert len(Dataset.merge_datasets(ob, li, la, remove_duplicates=False)) == 2395
+    assert len(Dataset.merge_datasets(ob, li, la)) == 2257
+
+
+
 
